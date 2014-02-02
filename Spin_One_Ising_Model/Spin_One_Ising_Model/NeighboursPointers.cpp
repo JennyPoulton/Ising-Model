15718ad3b26710722fc4440c84cd48be2259004d
JennyPoulton <jennypoulton@hotmail.co.uk>
2014-02-02 23:33:43 +0000

    A slight correction
    Still stuck on that one line

--- Spin_One_Ising_Model/Spin_One_Ising_Model/NeighboursPointers.cpp
+++ Spin_One_Ising_Model/Spin_One_Ising_Model/NeighboursPointers.cpp
@@ -9,21 +9,21 @@ int main (void)
 	int Left[ISINGSIZE*ISINGSIZE];		// the respective position in lattice
 	int Right[ISINGSIZE*ISINGSIZE];
 
-	int *Lattice[ISINGSIZE*ISINGSIZE]; //define the main lattice
+	int* Lattice[ISINGSIZE*ISINGSIZE]; //define the main lattice
 
-	for(float x=0; x<(ISINGSIZE*ISINGSIZE); x++)
+	for(int x=0; x<(ISINGSIZE*ISINGSIZE); x++)
 	{
-		Lattice[x] = (int)(pow(-1,x)); //set the main values
+		Lattice[x] = (int)(pow(-1,(float)x)); //set the main values
 	}
 
 
 	for(int x=0; x<(ISINGSIZE*ISINGSIZE); x++)
 	{
 
-		&Up[x]=*Lattice[((ISINGSIZE*ISINGSIZE)+x-ISINGSIZE)%(ISINGSIZE*ISINGSIZE)];		//set the surrounding lattice values 
-		&Down[x]=*Lattice[((ISINGSIZE*ISINGSIZE)+x+ISINGSIZE)%(ISINGSIZE*ISINGSIZE)];	// equal to those in correct positions
-		&Left[x]=*Lattice[((ISINGSIZE*ISINGSIZE)+x-1)%(ISINGSIZE*ISINGSIZE)];			// in lattice
-		&Right[x]=*Lattice[((ISINGSIZE*ISINGSIZE)+x+1)%(ISINGSIZE*ISINGSIZE)];
+		Up[x]=*Lattice[((ISINGSIZE*ISINGSIZE)+x-ISINGSIZE)%(ISINGSIZE*ISINGSIZE)];		//set the surrounding lattice values 
+		Down[x]=*Lattice[((ISINGSIZE*ISINGSIZE)+x+ISINGSIZE)%(ISINGSIZE*ISINGSIZE)];	// equal to those in correct positions
+		Left[x]=*Lattice[((ISINGSIZE*ISINGSIZE)+x-1)%(ISINGSIZE*ISINGSIZE)];			// in lattice
+		Right[x]=*Lattice[((ISINGSIZE*ISINGSIZE)+x+1)%(ISINGSIZE*ISINGSIZE)];
 
 	}
 
